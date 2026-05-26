b64538e86d4de61c585e775f0f6c6f63811dce10
MavorsCodes <164758446+MavorsCodes@users.noreply.github.com>
2026-05-26 02:26:52 +0200

    added startup script

---
+++ startup_scripts/main.js
@@ -0,0 +1,6 @@
+console.log("[GLOBALSETTER] I'm setting the global scope stuff in this project!")
+global.recipeTypes = {
+  'charging': 'createaddition:charging',
+  'energising': 'create_new_age:energising',
+  'deploying': 'create:deploying'
+};
