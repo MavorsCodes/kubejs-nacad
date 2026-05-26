e6779a4e5578cf43d30884f185257d59876c7a4c
MavorsCodes <164758446+MavorsCodes@users.noreply.github.com>
2026-05-26 02:28:45 +0200

    Delete startup_scripts directory

--- startup_scripts/main.js
+++
@@ -1,6 +0,0 @@
-console.log("[GLOBALSETTER] I'm setting the global scope stuff in this project!")
-global.recipeTypes = {
-  'charging': 'createaddition:charging',
-  'energising': 'create_new_age:energising',
-  'deploying': 'create:deploying'
-};
